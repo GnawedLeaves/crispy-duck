836bea77fce5bd41115131de7f18d3c77b446a24
Marcel :) <marcelyap31@gmail.com>
2026-08-26 11:26:23 +0800

    fix: moving navbar bug

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -2,15 +2,12 @@
 
 import { useAuth } from "@/app/context/AuthContext";
 import { token } from "@/app/theme";
-import {
-  ChartColumn,
-  ScanLine,
-  Users
-} from "lucide-react";
+import { ChartColumn, ScanLine, Users } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ViewTransition } from "react";
 import styles from "./navbar.module.css";
+
 const Navbar = () => {
   const { user } = useAuth();
   const pathname = usePathname();
@@ -20,25 +17,18 @@ const Navbar = () => {
     if (res) return true;
     return false;
   };
+
   if (user && !pathname.includes("/login")) {
     return (
       <ViewTransition name="footer">
-        <div className="fixed w-full h-fit px-2 py-12 bottom-0 flex items-center justify-center z-10">
+        <div className="fixed inset-x-0 bottom-0 flex items-center justify-center pb-6 px-2 z-50 pointer-events-none">
           <div
-            className={`cardWithShadow flex gap-8 items-center justify-center rounded-3xl border-2 `}
+            className="cardWithShadow flex gap-8 items-center justify-center rounded-3xl border-2 pointer-events-auto"
             style={{
               backgroundColor: token.light.background,
               borderColor: token.light.borderColor,
             }}
           >
-            {/* <div
-            className={`${styles.navbarItem}  ${isActive("/") ? styles.selectedNavItem : styles.unselectedNavItem}`}
-          >
-            <Link href={"/"}>
-              <House />
-            </Link>
-          </div> */}
-
             <div
               className={`${styles.navbarItem} ${isActive("/stats") ? styles.selectedNavItem : styles.unselectedNavItem}`}
             >
@@ -47,26 +37,19 @@ const Navbar = () => {
               </Link>
             </div>
             <div
-              className={`${styles.navbarItem}  ${isActive("/scan") ? styles.selectedNavItem : styles.unselectedNavItem}`}
+              className={`${styles.navbarItem} ${isActive("/scan") ? styles.selectedNavItem : styles.unselectedNavItem}`}
             >
               <Link href={"/scan"}>
                 <ScanLine />
               </Link>
             </div>
             <div
-              className={`${styles.navbarItem}  ${isActiveMany(["/friends", "/profile"]) ? styles.selectedNavItem : styles.unselectedNavItem}`}
+              className={`${styles.navbarItem} ${isActiveMany(["/friends", "/profile"]) ? styles.selectedNavItem : styles.unselectedNavItem}`}
             >
               <Link href={"/friends"}>
                 <Users />
               </Link>
             </div>
-            {/* <div
-            className={`${styles.navbarItem}  ${isActive("/profile") ? styles.selectedNavItem : styles.unselectedNavItem}`}
-          >
-            <Link href={"/profile"}>
-              <CircleUserRound />
-            </Link>
-          </div> */}
           </div>
         </div>
       </ViewTransition>
